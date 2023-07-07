6671712691b2bdeb55dfb717552e3e968e58df20
Avdeyy <60784831+Avdeyy@users.noreply.github.com>
2023-07-07 11:27:42 +0700

    Добавьте файлы проекта.

---
+++ Практическая №9/Class1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практическая__9
+{
+
+    public abstract class Discipline
+    {
+        public string? NameOfDis;
+        public abstract string Check(Student st, Discipline dp);
+    }
+
+    public interface IHaveAngryTeacher
+    {
+
+    }
+
+    public interface IHavePractice
+    {
+        int PracticeCount { get; set; }
+    }
+
+    public interface IHaveFinalControll
+    {
+        double PassingScore { get; set; }
+    }
+
+    public class Mathanalysis : Discipline, IHaveFinalControll
+    {
+        public double PassingScore { get; set; }
+
+        public Mathanalysis(double passingScore, string Name)
+        {
+            PassingScore = passingScore;
+            NameOfDis = Name;
+        }
+        public override string Check(Student st, Discipline dp)
+        {
+            if (st.FinalControll[dp] >= PassingScore)
+                return $"{st.Name} получил достаточный балл на итоговой аттестации и может расчитывать на получение автомата\n";
+
+            return $"{st.Name} не получил достаточный балл на итоговой аттестации и НЕ может расчитывать на получение автомата\n";
+        }
+    }
+    public class History : Discipline, IHavePractice, IHaveAngryTeacher
+    {
+        public int PracticeCount { get; set; }
+
+        public History(int passingScore, string Name)
+        {
+            PracticeCount = passingScore;
+            NameOfDis = Name;
+        }
+        public override string Check(Student st, Discipline dp)
+        {
+            if (st.Practices[dp] >= PracticeCount)
+                return $"{st.Name} успел сдать все практические работы вовремя и может расчитывать на получение автомата\n";
+
+            return $"{st.Name} не успел сдать все практические работы вовремя и НЕ может расчитывать на получение автомата\n";
+        }
+    }
+    public class Programming : Discipline, IHavePractice
+    {
+        public int PracticeCount { get; set; }
+
+        public Programming(int passingScore, string Name)
+        {
+            PracticeCount = passingScore;
+            NameOfDis = Name;
+        }
+        public override string Check(Student st, Discipline dp)
+        {
+            if (st.Practices[dp] >= PracticeCount)
+                return $"{st.Name} успел сдать все практические работы вовремя и может расчитывать на получение автомата\n";
+
+            return $"{st.Name} не успел сдать все практические работы вовремя и НЕ может расчитывать на получение автомата\n";
+        }
+    }
+    public class Student
+    {
+        public List<Student> Students = new List<Student>();
+        public List<Discipline> Disciplines = new List<Discipline>();
+        public Dictionary<Discipline, double> FinalControll = new Dictionary<Discipline, double>();
+        public Dictionary<Discipline, double> Practices = new Dictionary<Discipline, double>();
+        public string? Name { get; set; }
+    }
+}
---
+++ Практическая №9/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Net.Http.Headers;
+using Практическая__9;
+Mathanalysis math = new Mathanalysis(4.7, "Математический анализ");
+History his = new History(4, "История");
+Programming prog = new Programming(8, "Программирование");
+
+Student s1 = new Student(); s1.Name = "Антон Мамонтов";
+Student s2 = new Student(); s2.Name = "Дмитрий Алабаев";
+Student s3 = new Student(); s3.Name = "Лиза Мартынова";
+Student s4 = new Student(); s4.Name = "Евгений Ожегов";
+
+s1.FinalControll.Add(math, 4.6); s1.Practices.Add(his, 3); s1.Practices.Add(prog, 9);
+s2.FinalControll.Add(math, 4.8); s2.Practices.Add(his, 5); s2.Practices.Add(prog, 8);
+s3.FinalControll.Add(math, 4.2); s3.Practices.Add(his, 2); s3.Practices.Add(prog, 5);
+
+Student st = new Student();
+st.Students.Add(s1);
+st.Students.Add(s2);
+st.Students.Add(s3);
+
+Student dp = new Student();
+dp.Disciplines.Add(math);
+dp.Disciplines.Add(his);
+dp.Disciplines.Add(prog);
+
+string fs = "";
+
+for (int i = 0; i < dp.Disciplines.Count; i++)
+{
+    fs += $"\n{dp.Disciplines[i].NameOfDis}\n<><><><><><><><><><><>\n";
+    if (dp.Disciplines[i] is IHaveAngryTeacher)
+    {
+        fs += "Этот препод не даёт автоматов\n";
+        continue;
+    }
+    for (int j = 0; j < st.Students.Count; j++)
+    {
+        fs += $"{dp.Disciplines[i].Check(st.Students[j], dp.Disciplines[i])}";
+    }
+}
+
+Console.WriteLine(fs);
